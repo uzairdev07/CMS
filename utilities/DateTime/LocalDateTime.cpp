a764bc13063a34e3476b9e547a233b0209993bcd
ozairprince07 <ozairprince07@gmail.com>
2022-04-11 07:32:27 -0700

    Custom LocalDateTime Completion

--- utilities/DateTime/LocalDateTime.cpp
+++ utilities/DateTime/LocalDateTime.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "LocalDateTime.h"
+
+LocalDateTime::LocalDateTime(Date &date, Time &time) {
+    setDateTime(date, time);
+}
+
+void LocalDateTime::setDateTime(Date &date, Time &time) {
+    date = Date();
+    time = Time();
+}
+
+void LocalDateTime::print() {
+    cout << date.getDay() << "-" << date.getMonth() << date.getYear()
+         << " " << time.getHour() << ":" << time.getMinute() << ":" << time.getSecond();
+}
+
+// ? Getters and Setters
+
+const Date &LocalDateTime::getDate() const {
+    return date;
+}
+
+void LocalDateTime::setDate(const Date &date) {
+    LocalDateTime::date = date;
+}
+
+const Time &LocalDateTime::getTime() const {
+    return time;
+}
+
+void LocalDateTime::setTime(const Time &time) {
+    LocalDateTime::time = time;
+}
--- utilities/DateTime/LocalDateTime.h
+++ utilities/DateTime/LocalDateTime.h
@@ -5,9 +5,34 @@
 #ifndef CMS_LOCALDATETIME_H
 #define CMS_LOCALDATETIME_H
 
+#include <iostream>
+#include "Date.h"
+#include "Time.h"
 
 class LocalDateTime {
+private:
+    // * Attributes
+    Date date;
+    Time time;
+public:
+    // * Default Constructor
+    LocalDateTime(Date &, Time &);
+    // * Overloaded Constructors
 
+    // * Methods
+
+    void setDateTime(Date &, Time &);
+    // ? Print Local Date Time
+    void print();
+
+    // ? Getters and Setters
+    const Date &getDate() const;
+
+    void setDate(const Date &date);
+
+    const Time &getTime() const;
+
+    void setTime(const Time &time);
 };
 
 
